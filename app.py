fb81f1d2461274576d39f4311b37eef63615fcfd
SHUBHA67 <113436795+SHUBHA67@users.noreply.github.com>
2024-01-18 12:29:36 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+from sklearn.linear_model import LogisticRegression
 
 model =  pickle.load(open('./ML_Model.pkl', 'rb'))
 
